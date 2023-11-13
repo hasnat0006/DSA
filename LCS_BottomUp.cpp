60bd3b403cf26176b8f9ceb35bb70c12d16a7677
Yusuf Reza Hasnat <118974654+hasnat0006@users.noreply.github.com>
2023-11-13 19:40:29 +0600

    Update LCS_BottomUp.cpp

--- LCS_BottomUp.cpp
+++ LCS_BottomUp.cpp
@@ -4,30 +4,8 @@
 
 #pragma GCC optimize("O3")
 #include <bits/stdc++.h>
-#ifndef ONLINE_JUDGE
-#include "C:\Users\Yusuf Reza Hasnat\OneDrive\Desktop\CP\debug.h"
-#else
-#define dbg(x...)
-#define dbgc(x...)
-#endif
-
 using namespace std;
 
-#define int long long
-#define float long double
-#define pb push_back
-#define vf(v) (v).begin(), (v).end()
-#define vr(v) (v).rbegin(), (v).rend()
-#define dosomic(x) fixed << setprecision(x)
-#define endl "\n"
-#define case(x) cout << "Case " << x << ": "
-#define YUSUF ios_base::sync_with_stdio(false),
-#define REZA cin.tie(NULL),
-#define HASNAT cout.tie(NULL)
-
-int mod = 1000000007;
-float pi = acos(-1);
-int inf = 1e18;
 int dp[1000][1000], mark[1000][1000];
 
 void print_string(int i, int j, string a) {
